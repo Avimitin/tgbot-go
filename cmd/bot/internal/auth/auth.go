1ed327aaada597c4e742c199157eef2eac293d0e
Avimitin <avimitin@gmail.com>
2020-09-26 23:44:10 +0800

    增加init方法加载yml文件

--- cmd/bot/internal/auth/auth.go
+++ cmd/bot/internal/auth/auth.go
@@ -1,6 +1,23 @@
 package auth
 
-var creator int = 649191333
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+var creator int
+
+func Init(filepath string) error {
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(file, &m)
+	fmt.Printf("m: %v", m)
+	return nil
+}
 
 func IsCreator(uid int) bool {
 	return uid == creator

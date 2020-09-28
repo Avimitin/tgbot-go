0076ed9d199b359b8d24cd24c0e5d249e23af470
Avimitin <avimitin@gmail.com>
2020-09-28 21:08:38 +0800

    更改包结构，将 ini 改为 yaml

--- cmd/bot/Bot-Initialize.go
+++ cmd/bot/Bot-Initialize.go
@@ -1,37 +1,19 @@
-package main
+package bot
 
 import (
+	"github.com/Avimitin/go-bot/cmd/bot/internal/auth"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
-
-	"gopkg.in/ini.v1"
-
-	"log"
 )
 
-func loadCfg() (string, error) {
-	cfg, err := ini.Load("cfg/bot.ini")
-
-	if err != nil {
-		return "Some error occur.", err
-	}
-
-	token := cfg.Section("privacy").Key("token").String()
-
-	return token, nil
+func NewBot(token string) *tgbotapi.BotAPI {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil { panic(err) }
+	return bot
 }
 
-func NewBot() *tgbotapi.BotAPI {
-	token, err := loadCfg()
-
-	if err != nil {
-		log.Panicf("Some error occur when loading config.\n Descriptions: %v", err)
-	}
-
-	bot, err := tgbotapi.NewBotAPI(token)
-
-	if err != nil {
-		log.Panicf("Some error occur when initialize bot.\n Descriptions: %v", err)
-	}
-
-	return bot
+func NewCFG() *auth.Config {
+	config, err := auth.NewCFG()
+	if err != nil { panic(err) }
+	if config.LOADED != true { panic("Fail to load config")}
+	return &config
 }

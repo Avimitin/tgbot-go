d5d86dfbc13d34b7366a18347bfb9fd56315d57d
Avimitin <avimitin@gmail.com>
2020-09-28 21:06:42 +0800

    Create LoadConf.go

---
+++ cmd/bot/internal/auth/LoadConf.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Config struct {
+	LOADED		bool		`yaml:"LOADED"`
+	Groups   	[]int		`yaml:"groups"`
+	BotToken 	string		`yaml:"bot_token"`
+}
+
+func NewCFG() (Config, error) {
+	cfg := Config{}
+	err := decode(&cfg, "F:\\go-bot\\cfg\\auth.yml")
+	return cfg, err
+}
+
+func decode(cfg *Config,filepath string) error {
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil { return err }
+	return yaml.Unmarshal(file, cfg)
+}
+

968ebaf8291b6073c8065190b6f6e3246150c71b
Avimitin <avimitin@gmail.com>
2020-09-29 22:40:17 +0800

    增加自定义Error，增加获取群组admin方法

--- cmd/bot/internal/auth/auth.go
+++ cmd/bot/internal/auth/auth.go
@@ -1,5 +1,15 @@
 package auth
 
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+
+type MyError struct {
+	info string
+}
+
+func (e *MyError) Error() string {
+	return e.info
+}
+
 func IsCreator (creator int,uid int) bool {
 	return uid == creator
 }
@@ -9,4 +19,34 @@ func (cfg Config) IsAuthGroups (gid int64) bool {
 		return gid == authGid
 	}
 	return false
+}
+
+func getAdmin(bot *tgbotapi.BotAPI, chat *tgbotapi.Chat, c chan []int) {
+	members, err := bot.GetChatAdministrators(chat.ChatConfig())
+	if err != nil {
+		c <- nil
+		close(c)
+	}
+	admins := make([]int, len(members))
+	for i, member := range members {
+		admins[i] = member.User.ID
+	}
+	c <- admins
+}
+
+func IsAdmin (uid int, bot *tgbotapi.BotAPI, chat *tgbotapi.Chat) (bool, error) {
+	c := make(chan []int)
+	go getAdmin(bot, chat, c)
+	admins := <- c
+
+	if admins == nil {
+		return false, &MyError{info: "Error fetching admin"}
+	}
+
+	for _, admin := range admins {
+		if uid == admin {
+			return true, nil
+		}
+	}
+	return false, nil
 }

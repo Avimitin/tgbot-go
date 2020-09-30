877f2ee0ac3149fae0e0afea3b97f010982da509
Avimitin <avimitin@gmail.com>
2020-09-30 19:41:38 +0800

    Create CalcTime.go

---
+++ utils/modules/timer/CalcTime.go
@@ -0,0 +1,32 @@
+package timer
+
+import "time"
+
+type WrongInput struct {
+	specification string
+}
+
+func (w *WrongInput) Error() string {
+	return w.specification
+}
+
+func CalcTime(add int64, unit string,) (int64, error) {
+	now := time.Now().Unix()
+
+	contrast := map[string]int64{
+		"s": 1,
+		"m": 60,
+		"h": 3600,
+		"d": 86400,
+		"w": 604800,
+		"M": 2419200,
+		"y": 885427200,
+	}
+
+	if magnification, exist := contrast[unit]; exist != false {
+		add = add * magnification
+		return now + add, nil
+	}
+
+	return 0, &WrongInput{"Unknown unit"}
+}

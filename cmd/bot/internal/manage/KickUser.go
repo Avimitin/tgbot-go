fe2a7724daa4da165385a872379edd6d796aa65a
Avimitin <avimitin@gmail.com>
2020-09-30 19:42:58 +0800

    增加友好的错误回复

--- cmd/bot/internal/manage/KickUser.go
+++ cmd/bot/internal/manage/KickUser.go
@@ -20,7 +20,14 @@ func KickUser(bot *tgbotapi.BotAPI, uid int, cid int64, restrictTime int64) (tgb
 	var msg tgbotapi.MessageConfig
 
 	if err != nil {
-		msg = tgbotapi.NewMessage(cid, fmt.Sprintf("发生错误：%v\n\nHint: 您是不是忘记给Bot设置管理员权限了？", err))
+		switch err.Error() {
+		case "Bad Request: user is an administrator of the chat":
+			msg = tgbotapi.NewMessage(cid, "对方是管理员，我踢不掉！")
+		case "Bad Request: not enough rights to restrict/unrestrict chat member":
+			msg = tgbotapi.NewMessage(cid, "我没有管理员权限！")
+		default:
+			msg = tgbotapi.NewMessage(cid, fmt.Sprintf("发生错误：%v\n\nHint: 您是不是忘记给Bot设置管理员权限了？", err))
+		}
 	} else {
 		msg = tgbotapi.NewMessage(cid, fmt.Sprintf("成功踢出 %v", uid))
 	}

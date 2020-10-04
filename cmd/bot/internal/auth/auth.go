e140b8b8fdb5e2143403c3de194aca725ebd1e5b
Avimitin <avimitin@gmail.com>
2020-10-04 22:14:54 +0800

    使用本地数据库加快管理员认证

--- cmd/bot/internal/auth/auth.go
+++ cmd/bot/internal/auth/auth.go
@@ -1,6 +1,9 @@
 package auth
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+import (
+	"github.com/Avimitin/go-bot/cmd/bot/internal/database"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 type MyError struct {
 	info string
@@ -10,12 +13,12 @@ func (e *MyError) Error() string {
 	return e.info
 }
 
-func IsCreator (creator int,uid int) bool {
+func IsCreator(creator int, uid int) bool {
 	return uid == creator
 }
 
-func (cfg Config) IsAuthGroups (gid int64) bool {
-	for _,authGid := range cfg.Groups {
+func (cfg Config) IsAuthGroups(gid int64) bool {
+	for _, authGid := range cfg.Groups {
 		return gid == authGid
 	}
 	return false
@@ -34,12 +37,10 @@ func getAdmin(bot *tgbotapi.BotAPI, chat *tgbotapi.Chat, c chan []int) {
 	c <- admins
 }
 
-func IsAdmin (uid int, bot *tgbotapi.BotAPI, chat *tgbotapi.Chat) (bool, error) {
-	c := make(chan []int)
-	go getAdmin(bot, chat, c)
-	admins := <- c
+func IsAdmin(uid int, chat *tgbotapi.Chat) (bool, error) {
+	admins, err := database.GetAdmin(chat.UserName)
 
-	if admins == nil {
+	if admins == nil || err != nil {
 		return false, &MyError{info: "Error fetching admin"}
 	}
 

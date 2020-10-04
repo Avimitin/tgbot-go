78b70577592111f32de9bc90f1145533e55ff266
Avimitin <avimitin@gmail.com>
2020-10-04 21:37:34 +0800

    Create CreateGroup.go

---
+++ cmd/bot/internal/database/CreateGroup.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"fmt"
+	"log"
+)
+
+// Use this methods to create a new table
+func CreateGroup(groupName string) error {
+	db, err := connect()
+	defer db.Close()
+	if err != nil {
+		log.Printf("[Database]%v\n", err)
+		return err
+	}
+
+	sqlQuery := fmt.Sprintf(`
+		CREATE TABLE IF NOT EXISTS %v (
+		uid 		INT NOT NULL,
+		username 	VARCHAR(255) NOT NULL, 
+		permission  VARCHAR(10) NOT NULL DEFAULT 'member',
+		PRIMARY KEY (uid)
+		) DEFAULT CHARSET=utf8`, groupName)
+
+	stmt, err := db.Prepare(sqlQuery)
+	if err != nil {
+		log.Printf("[Database]%v\n", err)
+		return err
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Printf("[Database]%v\n", err)
+		return err
+	}
+
+	return nil
+}

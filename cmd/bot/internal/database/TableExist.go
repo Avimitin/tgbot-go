bf19ceafe954119503ac584502ebe9a89e3a1796
Avimitin <avimitin@gmail.com>
2020-10-04 21:38:00 +0800

    Create TableExist.go

---
+++ cmd/bot/internal/database/TableExist.go
@@ -0,0 +1,24 @@
+package database
+
+func TableExist(tableName string) (bool, error) {
+	db, err := connect()
+	if err != nil {
+		return false, err
+	}
+	result, err := db.Query("SHOW TABLES")
+	if err != nil {
+		return false, err
+	}
+	for result.Next() {
+		var table string
+		err = result.Scan(&table)
+		if err != nil {
+			return false, err
+		}
+
+		if tableName == table {
+			return true, nil
+		}
+	}
+	return false, nil
+}

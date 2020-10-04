3d0e69e5fb3ac3efdd765f3b95dde01f4c517a4b
Avimitin <avimitin@gmail.com>
2020-10-04 22:13:39 +0800

    Create GetAdmin.go

---
+++ cmd/bot/internal/database/GetAdmin.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"fmt"
+)
+
+func GetAdmin(groupName string) ([]int, error) {
+	db, err := connect()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlQuery := fmt.Sprintf(`SELECT uid FROM %v WHERE permission="admin"`, groupName)
+
+	stmt, err := db.Prepare(sqlQuery)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	response, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+
+	var adminList []int
+	for response.Next() {
+		var result int
+		err := response.Scan(&result)
+		if err != nil {
+			return nil, err
+		}
+		adminList = append(adminList, result)
+	}
+
+	return adminList, nil
+}

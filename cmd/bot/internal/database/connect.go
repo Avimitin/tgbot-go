e36a97d62ea6b0f5aeb1ad267bda09fb9584e47c
Avimitin <avimitin@gmail.com>
2020-10-05 17:06:36 +0800

    使用全局变量创建复用连接池

--- cmd/bot/internal/database/connect.go
+++ cmd/bot/internal/database/connect.go
@@ -3,25 +3,46 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"github.com/Avimitin/go-bot/cmd/bot/internal/auth"
+	"github.com/Avimitin/go-bot/cmd/bot/internal/CFGLoader"
 	_ "github.com/go-sql-driver/mysql"
 	"time"
 )
 
-func connect() (*sql.DB, error) {
-	cfg, err := auth.NewCFG()
+var (
+	DB       sql.DB
+	hasSetUp bool
+)
+
+func register(database *sql.DB) error {
+	cfg, err := CFGLoader.LoadCFG()
 	if err != nil {
-		return nil, err
+		return err
 	}
-	db, err := sql.Open("mysql",
+	database, err = sql.Open("mysql",
 		fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8", cfg.DBCfg.User, cfg.DBCfg.Password, cfg.DBCfg.Host, cfg.DBCfg.Database))
 
 	// Set limit
-	if db != nil {
-		db.SetConnMaxLifetime(time.Minute * 3)
-		db.SetMaxOpenConns(10)
-		db.SetMaxIdleConns(10)
+	if database != nil {
+		database.SetConnMaxLifetime(time.Minute * 3)
+		database.SetMaxOpenConns(10)
+		database.SetMaxIdleConns(10)
 	}
 
-	return db, db.Ping()
+	return nil
+}
+
+func connect() (*sql.DB, error) {
+	err := register(&DB)
+	if err != nil {
+		return nil, err
+	}
+	hasSetUp = true
+	return &DB, nil
+}
+
+func NewDB() (*sql.DB, error) {
+	if !hasSetUp {
+		return connect()
+	}
+	return &DB, nil
 }

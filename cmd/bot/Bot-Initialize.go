f0708fed45370179ef63daeef4ee312ac035d8d7
Avimitin <avimitin@gmail.com>
2020-10-08 16:35:49 +0800

    增加认证群组初始化功能；修改地址值返回方式；出现错误时增加log报错

--- cmd/bot/Bot-Initialize.go
+++ cmd/bot/Bot-Initialize.go
@@ -5,12 +5,13 @@ import (
 	"github.com/Avimitin/go-bot/cmd/bot/internal/CFGLoader"
 	"github.com/Avimitin/go-bot/cmd/bot/internal/database"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
 )
 
 func NewBot(token string) *tgbotapi.BotAPI {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	return bot
 }
@@ -18,18 +19,30 @@ func NewBot(token string) *tgbotapi.BotAPI {
 func NewCFG() *CFGLoader.Config {
 	config, err := CFGLoader.LoadCFG()
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	if config.LOADED != true {
-		panic("Fail to load config")
+		log.Panic("Fail to load config")
 	}
-	return &config
+	return config
 }
 
 func NewDB() *sql.DB {
-	DB, err := database.NewDB()
+	DB, err := database.NewDB(cfg)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	return DB
+}
+
+func NewAuthGroups() []int64 {
+	groups, err := database.SearchGroups(DB)
+	if err != nil {
+		log.Panic(err)
+	}
+	groupsID := make([]int64, len(groups))
+	for i, group := range groups {
+		groupsID[i] = group.GroupID
+	}
+	return groupsID
 }

b5d0ba2bea0d94dbd9a07c81f26946e5d9cc3e5a
Avimitin <avimitin@gmail.com>
2020-10-09 12:51:05 +0800

    增加keyword的数据表增删查改函数；修改peek为返回keywprd所在位置

--- cmd/bot/internal/database/Keywords.go
+++ cmd/bot/internal/database/Keywords.go
@@ -2,13 +2,14 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 )
 
-func PeekKeywords(DB *sql.DB, keyword string) (bool, error) {
-	rows, err := DB.Query("SELECT kid FROM keywords WHERE keywords=?", keyword)
+func PeekKeywords(DB *sql.DB, keyword string) (int, error) {
+	rows, err := DB.Query("SELECT kid FROM keywords WHERE keyword=?", keyword)
 	if err != nil {
 		Pln("Error Occur when preparing sql query. INFO:", err.Error())
-		return false, err
+		return -1, err
 	}
 	defer rows.Close()
 	var peek int
@@ -17,11 +18,94 @@ func PeekKeywords(DB *sql.DB, keyword string) (bool, error) {
 	}
 	if err != nil {
 		Pln("Error Occur when scanning data. INFO:", err.Error())
-		return false, err
+		return -1, err
 	}
 
 	if peek != 0 {
-		return true, nil
+		return peek, nil
 	}
-	return false, nil
+	return -1, nil
+}
+
+func AddKeywords(DB *sql.DB, keyword string, reply string) error {
+	// peek if keyword has existed or not.
+	kid, err := PeekKeywords(DB, keyword)
+	if err != nil {
+		return err
+	}
+
+	// if keyword has existed, add new reply.
+	if kid != -1 {
+		return SetReply(DB, reply, kid)
+	}
+
+	// If keyword isn't exist, insert keyword first.
+	stmt, err := DB.Prepare("INSERT INTO keywords (keyword) VALUES (?)")
+	if err != nil {
+		Pln("Error occur when preparing insert. Info:", err.Error())
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(keyword)
+	if err != nil {
+		Pln("Error occur when executive value. Info:", err.Error())
+		return err
+	}
+	ID, _ := result.LastInsertId()
+	Pln(fmt.Sprintf("Successfully insert keyword. Insert ID: %v", ID))
+	// Then insert reply.
+	return SetReply(DB, reply, kid)
+}
+
+func RenameKeywords(DB *sql.DB, kid int, newKeyword string) error {
+	stmt, err := DB.Prepare("UPDATE keywords SET keyword = ? WHERE kid = ?")
+	if err != nil {
+		Pln("Error occur when preparing update keyword. Info:", err.Error())
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(newKeyword, kid)
+	if err != nil {
+		Pln("Error occur when executive value. Info:", err.Error())
+		return err
+	}
+	row, _ := result.RowsAffected()
+	Pln(fmt.Sprintf("Rename keyword successfully. Affected %v rows", row))
+	return nil
+}
+
+func DelKeyword(DB *sql.DB, k int) error {
+	// Delete reply relate with keyword first.
+	stmt, err := DB.Prepare("DELETE FROM replies WHERE kid = ?")
+	if err != nil {
+		Pln("Error occur when preparing delete keyword. Info:", err.Error())
+		return err
+	}
+	result, err := stmt.Exec(k)
+	if err != nil {
+		Pln("Error occur when executive value. Info:", err.Error())
+		return err
+	}
+
+	row, _ := result.RowsAffected()
+	Pln(fmt.Sprintf("Delete replies successfully. Affected %v rows", row))
+
+	// Delete keyword
+	stmt, err = DB.Prepare("DELETE FROM keywords WHERE kid = ?")
+	if err != nil {
+		Pln("Error occur when preparing delete keyword. Info:", err.Error())
+		return err
+	}
+	defer stmt.Close()
+
+	result, err = stmt.Exec(k)
+	if err != nil {
+		Pln("Error occur when executive value. Info:", err.Error())
+		return err
+	}
+
+	row, _ = result.RowsAffected()
+	Pln(fmt.Sprintf("Delete keyword successfully. Affected %v rows", row))
+	return nil
 }

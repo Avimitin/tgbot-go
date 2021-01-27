9f0d3c68c6f81497a9706a1e1619379a8d8faaa6
avimitin <avimitin@gmail.com>
2021-01-27 21:55:01 +0800

    store current work

--- internal/pkg/conf/ConfType.go
+++ internal/pkg/conf/ConfType.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"fmt"
+	"io/ioutil"
 )
 
 // DBSecret store database DSN information
@@ -16,4 +17,12 @@ type DBSecret struct {
 // MySqlURL return formatted mysql tcp link
 func (db *DBSecret) MySqlURL() string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", db.user, db.pwd, db.host, db.port, db.database)
+}
+
+type Configuration struct {
+	CertGroup []int64        `json:"cert_group"`
+	Users     map[int]string `json:"users"`
+}
+
+func NewConfiguration(cfgPath string) (*Configuration, error) {
 }

cb9e9d3e01a3f2c21caa8d7f1d00b007a2efbc63
avimitin <avimitin@gmail.com>
2021-01-27 21:55:01 +0800

    Add configuration files
    - add configuration struct
    - add funtion to return initialized struct

--- internal/pkg/conf/ConfType.go
+++ internal/pkg/conf/ConfType.go
@@ -1,7 +1,9 @@
 package conf
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
 // DBSecret store database DSN information
@@ -16,4 +18,22 @@ type DBSecret struct {
 // MySqlURL return formatted mysql tcp link
 func (db *DBSecret) MySqlURL() string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", db.user, db.pwd, db.host, db.port, db.database)
+}
+
+type Configuration struct {
+	CertGroup []int64        `json:"cert_group"`
+	Users     map[int]string `json:"users"`
+}
+
+func NewConfiguration(cfgPath string) (*Configuration, error) {
+	files, err := ioutil.ReadFile(cfgPath)
+	if err != nil {
+		return nil, fmt.Errorf("can't read configuration JSON file: %v", err)
+	}
+	var cfg *Configuration
+	err = json.Unmarshal(files, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("Fail to unmarshal configuration data: %v", err)
+	}
+	return cfg, nil
 }

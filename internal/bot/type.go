20dc19f21620dcfd2dc558d7a99692d186215d00
avimitin <avimitin@gmail.com>
2021-01-29 10:20:32 +0000

    add convenient edit function and logging function

--- internal/bot/type.go
+++ internal/bot/type.go
@@ -1,6 +1,9 @@
 package bot
 
 import (
+	"fmt"
+	"log"
+
 	bapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
@@ -21,4 +24,14 @@ func sendP(text string, chatID int64, parseMode string) (bapi.Message, error) {
 	msg := bapi.NewMessage(chatID, text)
 	msg.ParseMode = parseMode
 	return bot.Send(msg)
+}
+
+func editT(newText string, chatID int64, msgID int) (bapi.Message, error) {
+	msg := bapi.NewEditMessageText(chatID, msgID, newText)
+	return bot.Send(msg)
+}
+
+func errF(where string, err error, more string) error {
+	log.Printf("[%s]%v", where, err)
+	return fmt.Errorf("%s:\n%w", more, err)
 }

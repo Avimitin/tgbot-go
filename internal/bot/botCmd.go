0f5e7bcebf59dcec9082dba008c5b9ffe06fe96d
avimitin <avimitin@gmail.com>
2021-01-29 10:19:16 +0000

    add ping command support and use mini method to handle error
    - add ping command
    - use errF function instead of log.Printf and fmt.Errorf

--- internal/bot/botCmd.go
+++ internal/bot/botCmd.go
@@ -2,8 +2,8 @@ package bot
 
 import (
 	"fmt"
-	"log"
 	"strings"
+	"time"
 
 	bapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -30,8 +30,22 @@ func start(m *bapi.Message) error {
 	userLink := fmt.Sprintf(`<a href="tg://user?id=%d">%s</a>`, userID, username)
 	_, err := sendT("Hi, "+userLink, m.Chat.ID)
 	if err != nil {
-		log.Println("[start]", err)
-		return fmt.Errorf("%v", err)
+		return errF("start", err, "fail to send message")
+	}
+	return nil
+}
+
+func ping(m *bapi.Message) error {
+	now := time.Now()
+	msg, err := sendT("pong!", m.Chat.ID)
+	if err != nil {
+		return errF("ping", err, "fail to send msg")
+	}
+	delay := now.Sub(now).Milliseconds()
+	text := fmt.Sprintf("bot 与 Telegram 服务器的延迟大约为 %d 毫秒", delay)
+	_, err = editT(text, m.Chat.ID, msg.MessageID)
+	if err != nil {
+		return errF("ping", err, "fail to edit msg")
 	}
 	return nil
 }

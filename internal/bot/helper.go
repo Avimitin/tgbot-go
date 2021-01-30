b985617afcf86798563e86281a281d5dae45033a
avimitin <avimitin@gmail.com>
2021-01-30 10:16:34 +0000

    move tool function to helper.go

---
+++ internal/bot/helper.go
@@ -0,0 +1,29 @@
+package bot
+
+import (
+	"fmt"
+	"log"
+
+	bapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func sendT(text string, chatID int64) (bapi.Message, error) {
+	msg := bapi.NewMessage(chatID, text)
+	return bot.Send(msg)
+}
+
+func sendP(text string, chatID int64, parseMode string) (bapi.Message, error) {
+	msg := bapi.NewMessage(chatID, text)
+	msg.ParseMode = parseMode
+	return bot.Send(msg)
+}
+
+func editT(newText string, chatID int64, msgID int) (bapi.Message, error) {
+	msg := bapi.NewEditMessageText(chatID, msgID, newText)
+	return bot.Send(msg)
+}
+
+func errF(where string, err error, more string) error {
+	log.Printf("[%s]%v", where, err)
+	return fmt.Errorf("%s:\n%w", more, err)
+}

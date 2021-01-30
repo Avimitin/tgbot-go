8f7ba0d8a168a738e766b84d25f6850d18d02af9
avimitin <avimitin@gmail.com>
2021-01-30 10:31:42 +0000

    add dump message support

--- internal/bot/botCmd.go
+++ internal/bot/botCmd.go
@@ -49,3 +49,37 @@ func ping(m *bapi.Message) error {
 	}
 	return nil
 }
+
+func dump(m *bapi.Message) error {
+	var text = "<b>Message Information</b>\n" +
+		"=== <b>CHAT</b> ===\n" +
+		"<b>ID:</b> <code>%v</code>\n" +
+		"<b>TYPE:</b> <code>%v</code>\n" +
+		"<b>USERNAME:</b> <code>%v</code>\n" +
+		"=== <b>USER</b> ===\n" +
+		"<b>ID:</b> <code>%v</code>\n" +
+		"<b>USERNAME:</b> <code>%v</code>\n" +
+		"<b>NICKNAME:</b> <code>%v %v</code>\n" +
+		"<b>LANGUAGE:</b> <code>%v</code>\n" +
+		"=== <b>MSG</b> ===\n" +
+		"<b>ID:</b> <code>%v</code>\n" +
+		"<b>TEXT:</b> %v"
+
+	if reply := m.ReplyToMessage; reply != nil {
+		text = fmt.Sprintf(text,
+			reply.Chat.ID, reply.Chat.Type, reply.Chat.UserName,
+			reply.From.ID, reply.From.UserName, reply.From.FirstName, reply.From.LastName, reply.From.LanguageCode,
+			reply.MessageID, reply.Text)
+	} else {
+		text = fmt.Sprintf(text,
+			m.Chat.ID, m.Chat.Type, m.Chat.UserName,
+			m.From.ID, m.From.UserName, m.From.FirstName, m.From.LastName, m.From.LanguageCode,
+			m.MessageID, m.Text)
+	}
+
+	_, err := sendP(text, m.Chat.ID, "HTML")
+	if err != nil {
+		return errF("dump", err, "fail to send dump message")
+	}
+	return nil
+}

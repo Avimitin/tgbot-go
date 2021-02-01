8c285f830d9aa6509f1baab2d7ef6d4f20c3b89a
avimitin <avimitin@gmail.com>
2021-02-01 13:34:34 +0000

    added config test

---
+++ internal/bot/config_test.go
@@ -0,0 +1,51 @@
+package bot
+
+import (
+	"os"
+	"os/user"
+	"testing"
+)
+
+func TestWhereCFG(t *testing.T) {
+	const PATH string = "PATH/TO/CFG"
+	if path := WhereCFG(PATH); path != PATH {
+		t.Fatalf("Want %s got %s", PATH, path)
+	}
+
+	err := os.Setenv("BOTCFGPATH", PATH)
+	if err != nil {
+		t.Fatalf("Error happen when setting config path env")
+	}
+	if path := WhereCFG(""); path != PATH {
+		t.Fatalf("Env test fail. Want %s got %s", PATH, path)
+	}
+	err = os.Unsetenv("BOTCFGPATH")
+	if err != nil {
+		t.Log(err)
+	}
+
+	u, err := user.Current()
+	if err != nil {
+		t.Fatal(err)
+	}
+	dir := u.HomeDir + "/.config/go-bot"
+	err = os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if path := WhereCFG(""); path != dir {
+		t.Fatalf("home path test failed, want %s got %s", dir, path)
+	}
+}
+
+func TestNewConfig(t *testing.T) {
+	const PATH string = "../../cfg/config.json"
+	config := newConfigFromGivenPath(PATH)
+	if config == nil {
+		t.Errorf("got nil config")
+	}
+	if config.BotToken == "" {
+		t.Errorf("can't not read config")
+	}
+}

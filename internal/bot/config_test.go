0778217d5cb64cead274fe9d6d34f6c5b200574e
avimitin <avimitin@gmail.com>
2021-02-19 21:25:51 +0800

    deleted out of dated test

--- internal/bot/config_test.go
+++ internal/bot/config_test.go
@@ -1,10 +1,8 @@
 package bot
 
 import (
-	"io/ioutil"
 	"os"
 	"os/user"
-	"reflect"
 	"testing"
 )
 
@@ -39,63 +37,4 @@ func TestWhereCFG(t *testing.T) {
 	if path := WhereCFG(""); path != dir {
 		t.Fatalf("home path test failed, want %s got %s", dir, path)
 	}
-}
-
-func TestNewConfig(t *testing.T) {
-	const PATH string = "../../cfg"
-	config := newConfigFromGivenPath(PATH)
-	if config == nil {
-		t.Errorf("got nil config")
-	}
-	if config.BotToken == "" {
-		t.Errorf("can't not read config")
-	}
-}
-
-func TestDumpCFG(t *testing.T) {
-	cfg := Configuration{
-		BotToken:     "abc:123",
-		Groups: []int64{123, 456},
-	}
-
-	os.Setenv("BOTCFGPATH", "../../cfg")
-	if os.Getenv("BOTCFGPATH") != "../../cfg" {
-		t.Fatalf("fail to set environment variable")
-	}
-
-	err := cfg.DumpConfig()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	tCfg := NewConfig()
-	if tCfg.BotToken != cfg.BotToken {
-		t.Errorf("got %+v", tCfg)
-	}
-
-	os.Unsetenv("BOTCFGPATH")
-}
-
-func TestConfigSave(t *testing.T) {
-	cfg := Configuration{
-		BotToken: "abc:123",
-		certedGroups: map[int64]interface{}{
-			1234: struct{}{},
-		},
-	}
-
-	os.Setenv("BOTCFGPATH", "../../cfg")
-	cfg.save()
-	_, err := ioutil.ReadFile("../../cfg/config.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	nCFG := NewConfig()
-
-	if !reflect.DeepEqual(cfg.certedGroups, nCFG.certedGroups) {
-		t.Fatalf("got %+v", nCFG)
-	}
-
-	os.Unsetenv("BOTCFGPATH")
 }

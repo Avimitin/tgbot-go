e9d1c7d725f690015bb4a4b3b669538f92056ec5
avimitin <avimitin@gmail.com>
2021-04-28 18:10:33 +0800

    added permission descriptions

--- modules/database/user.go
+++ modules/database/user.go
@@ -1,5 +1,15 @@
 package database
 
+var (
+	permission = map[int32]string{
+		0: "owner",
+		1: "admin",
+		2: "channelManager",
+		3: "normal",
+		4: "ban",
+	}
+)
+
 //User describe a user
 type User struct {
 	ID int

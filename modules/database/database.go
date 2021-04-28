173a08908364180891fd62041bb3853dcf86a980
avimitin <avimitin@gmail.com>
2021-04-28 18:10:28 +0800

    added GetUser and NewUser

--- modules/database/database.go
+++ modules/database/database.go
@@ -19,3 +19,36 @@ func NewBotDB(dsn string) (*BotDB, error) {
 	}
 	return &BotDB{cnct: db}, nil
 }
+
+func (db *BotDB) GetUser(id int) (*User, error) {
+	var u User
+
+	result := db.cnct.First(&u, id)
+
+	switch result.Error {
+	case nil:
+		break
+	case gorm.ErrRecordNotFound:
+		return nil, fmt.Errorf("id %d not found", id)
+	default:
+		return nil, fmt.Errorf("get user %d: %w", id, result.Error)
+	}
+
+	return &u, nil
+}
+
+func (db *BotDB) NewUser(id int, permID int32) (*User, error) {
+	u := &User{
+		ID:       id,
+		PermID:   permID,
+		PermDesc: permission[permID],
+	}
+
+	result := db.cnct.Create(&u)
+
+	if result.Error != nil {
+		return nil, fmt.Errorf("create user %d: %w", id, result.Error)
+	}
+
+	return u, nil
+}

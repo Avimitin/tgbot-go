81d984082897e3ad8fc7acd282d83153488550c2
avimitin <avimitin@gmail.com>
2021-04-30 13:30:13 +0800

    added database log level configuration

--- modules/database/database.go
+++ modules/database/database.go
@@ -5,6 +5,7 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type BotDB struct {
@@ -12,8 +13,10 @@ type BotDB struct {
 }
 
 // NewBotDB return a abstract packaging database
-func NewBotDB(dsn string) (*BotDB, error) {
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+func NewBotDB(dsn string, logLevel string) (*BotDB, error) {
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(transLogLevel(logLevel)),
+	})
 	if err != nil {
 		return nil, fmt.Errorf("connect to %s: %w", dsn, err)
 	}
@@ -21,4 +24,19 @@ func NewBotDB(dsn string) (*BotDB, error) {
 	db.AutoMigrate(&User{})
 
 	return &BotDB{cnct: db}, nil
+}
+
+func transLogLevel(logLevel string) logger.LogLevel {
+	switch logLevel {
+	case "silent":
+		return logger.Silent
+	case "error":
+		return logger.Error
+	case "warn":
+		return logger.Warn
+	case "info":
+		return logger.Info
+	}
+
+	return logger.Silent
 }

37985aecb3b50b7510ad5d61c190e1583f85c5b0
avimitin <avimitin@gmail.com>
2021-06-18 18:52:42 +0800

    feat: added currency package

---
+++ modules/currency/currency.go
@@ -0,0 +1,185 @@
+package currency
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/Avimitin/go-bot/modules/net"
+)
+
+var (
+	buffer = struct {
+		currencies     map[string]string
+		lastUpdateTime time.Time
+		locker         sync.RWMutex
+		exchangeRate   map[string]map[string]float64
+	}{
+		currencies:   make(map[string]string),
+		exchangeRate: make(map[string]map[string]float64),
+	}
+)
+
+func StoreRateInBuffer(from, to string, rate float64) {
+	buffer.locker.Lock()
+	defer buffer.locker.Unlock()
+
+	buffer.exchangeRate[from] = map[string]float64{to: rate}
+}
+
+func BufferHasRate(from, to string) (float64, error) {
+	buffer.locker.RLock()
+	defer buffer.locker.RUnlock()
+
+	if codeTo, ok := buffer.exchangeRate[from]; ok {
+		if rate, ok := codeTo[to]; ok {
+			return rate, nil
+		}
+	}
+
+	return 0, fmt.Errorf("No data")
+}
+
+// StoreBuffer release buffer and replace the inner data with
+// given data.
+func StoreBuffer(currencies map[string]string) {
+	buffer.locker.Lock()
+	defer buffer.locker.Unlock()
+
+	// clean old buffer
+	if buffer.currencies != nil {
+		buffer.currencies = nil
+	}
+	buffer.currencies = currencies
+
+	// reset timer
+	buffer.lastUpdateTime = time.Now()
+}
+
+// BufferIsOutDated return true if It has been more than 24 hours since
+// the last cache being update.
+func BufferIsOutDated() bool {
+	buffer.locker.RLock()
+	defer buffer.locker.RUnlock()
+
+	if buffer.lastUpdateTime.IsZero() {
+		return true
+	}
+
+	return time.Now().Sub(buffer.lastUpdateTime).Hours() > 24
+}
+
+// BufferIsNil return true if the buffer is empty
+func BufferIsNil() bool {
+	buffer.locker.RLock()
+	defer buffer.locker.RUnlock()
+
+	return buffer.currencies == nil
+}
+
+// ListAllCurrenciesDescriptions return a map of the currency code and
+// currency code descriptions. Return error if request failed.
+func ListAllCurrenciesDescriptions() (map[string]string, error) {
+	if !BufferIsOutDated() && !BufferIsNil() {
+		return buffer.currencies, nil
+	}
+
+	response, err := net.Get("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.min.json")
+	if err != nil {
+		return nil, fmt.Errorf("request currency API: %w", err)
+	}
+
+	var currencies = make(map[string]string)
+	err = json.Unmarshal(response, &currencies)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshall currencies result: %w", err)
+	}
+
+	StoreBuffer(currencies)
+
+	return currencies, nil
+}
+
+// ListAllCurrencies return list of currency code that can be used for
+// calculate rate. Return error if ListAllCurrenciesDescriptions failed.
+func ListAllCurrencies() ([]string, error) {
+	currencies, err := ListAllCurrenciesDescriptions()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var currenciesKey = make([]string, 150)
+
+	for k := range currencies {
+		currenciesKey = append(currenciesKey, k)
+	}
+
+	return currenciesKey, nil
+}
+
+// Exchange fetch the rate about the two parameters. Valid parameters can be found
+// by ListAllCurrencies. Return error if request failed.
+func Exchange(from, to string) (map[string]interface{}, error) {
+	response, err := net.Get(
+		fmt.Sprintf(
+			"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/%s/%s.json",
+			from,
+			to,
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("request exchange api: %w", err)
+	}
+
+	var result map[string]interface{}
+
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return nil, fmt.Errorf("decode exchange result: %w", err)
+	}
+
+	return result, nil
+}
+
+func FetchRate(from, to string) (float64, error) {
+	if !BufferIsOutDated() {
+		if rate, err := BufferHasRate(from, to); err != nil {
+			return rate, nil
+		}
+	}
+
+	result, err := Exchange(from, to)
+	if err != nil {
+		return 0, err
+	}
+
+	rateRaw, have := result[to]
+	if !have {
+		return 0, fmt.Errorf("unexpect currency code %s", to)
+	}
+
+	rate, ok := rateRaw.(float64)
+	if !ok {
+		return 0, fmt.Errorf("illegal rate value")
+	}
+
+	StoreRateInBuffer(from, to, rate)
+
+	return rate, nil
+}
+
+// CalculateExchange convert the currency amount with given currency code.
+func CalculateExchange(amount float64, from, to string) (float64, error) {
+	rate, err := FetchRate(from, to)
+	if err != nil {
+		return 0, err
+	}
+
+	if rate <= 0 {
+		return 0, fmt.Errorf("rate is unexpected")
+	}
+
+	return amount * rate, nil
+}
---
+++ modules/currency/currency_test.go
@@ -0,0 +1,89 @@
+package currency
+
+import (
+	"testing"
+	"time"
+)
+
+func TestListAllCurrenciesDesc(t *testing.T) {
+	currencies, err := ListAllCurrenciesDescriptions()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var testShortName = "cny"
+	get := currencies[testShortName]
+	want := "Chinese Yuan"
+
+	if get != want {
+		t.Errorf("get %s want %s", get, want)
+	}
+}
+
+func TestListAvailableCurrencies(t *testing.T) {
+	currencies, err := ListAllCurrencies()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(currencies) == 0 {
+		t.Errorf("no currencies available")
+	}
+}
+
+func TestStoreBuffer(t *testing.T) {
+	var testBuffer = map[string]string{"foo": "bar"}
+	StoreBuffer(testBuffer)
+
+	for key, value := range buffer.currencies {
+		if testBuffer[key] != value {
+			t.Errorf("got %s want %s", value, testBuffer[key])
+		}
+	}
+}
+
+func TestBufferOutDated(t *testing.T) {
+	// let buffer time behind test time
+	buffer.lastUpdateTime = time.Now().AddDate(0, 0, -2)
+	if !BufferIsOutDated() {
+		t.Errorf("buffer should out of dated")
+	}
+}
+
+func TestExchange(t *testing.T) {
+	var (
+		currencyFixtures = []string{"cny", "usd"}
+	)
+	result, err := Exchange(currencyFixtures[0], currencyFixtures[1])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	date, ok := result["date"].(string)
+	if !ok {
+		t.Error("response is not expect")
+	}
+
+	data, ok := result[currencyFixtures[1]].(float64)
+	if !ok {
+		t.Error("response is not expect")
+	}
+
+	if date == "" || data == 0 {
+		t.Errorf("unexpected date %s and data %f", date, data)
+	}
+}
+
+func TestCalculate(t *testing.T) {
+	result, err := CalculateExchange(100, "cny", "usd")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if result == 0 {
+		t.Fatal("no data calculated")
+	}
+
+	t.Log(result)
+}

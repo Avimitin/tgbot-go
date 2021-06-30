ec23d94905ccb1055f46fa8f0d15e8fba927834f
avimitin <avimitin@gmail.com>
2021-06-30 17:04:30 +0800

    fix: fixed new mark would overwrite old mark issue

--- modules/mark/mark.go
+++ modules/mark/mark.go
@@ -2,23 +2,37 @@ package mark
 
 import (
 	"errors"
+	"math"
 	"sync"
 )
 
-type mark struct {
-	id          int32
-	link        string
-	description string
+// Mark store information about a mark
+type Mark struct {
+	owner       string
+	ID          int32
+	Link        string
+	Description string
+}
+
+func newMark(id int32, owner, link, description string) Mark {
+	return Mark{
+		ID:          id,
+		owner:       owner,
+		Link:        link,
+		Description: description,
+	}
 }
 
 var (
 	innerDB = struct {
-		db     map[string][]mark
+		db     map[string][]Mark
 		lock   sync.RWMutex
 		amount int32
+		limit  int32
 	}{
-		db:     map[string][]mark{},
+		db:     map[string][]Mark{},
 		amount: 0,
+		limit:  math.MaxInt32,
 	}
 )
 
@@ -27,29 +41,32 @@ func AddMark(user, link, description string) error {
 	innerDB.lock.Lock()
 	defer innerDB.lock.Unlock()
 
-	basicMark := mark{
-		id:          0,
-		link:        link,
-		description: description,
+	if innerDB.amount >= innerDB.limit {
+		return errors.New("storage over limit")
 	}
+
+	basicMark := newMark(0, user, link, description)
 
 	if linkList, ok := innerDB.db[user]; ok {
 		if length := len(linkList); length > 0 {
-			basicMark.id = linkList[length-1].id + 1
+			basicMark.ID = linkList[length-1].ID + 1
 			linkList = append(linkList, basicMark)
 		} else {
 			linkList = append(linkList, basicMark)
 		}
+		innerDB.db[user] = linkList
+	} else {
+		innerDB.db[user] = []Mark{basicMark}
 	}
 
-	innerDB.db[user] = []mark{basicMark}
+	innerDB.amount++
 
 	return nil
 }
 
 // GetMark search links from given user name.
 // Return links if user exist, return error if user not found.
-func GetMark(user string) ([]mark, error) {
+func GetMark(user string) ([]Mark, error) {
 	innerDB.lock.RLock()
 	defer innerDB.lock.RUnlock()
 
@@ -74,8 +91,9 @@ func DelMark(user string, target int32) error {
 
 	marks := innerDB.db[user]
 	for i, mark := range marks {
-		if mark.id == target {
+		if mark.ID == target {
 			innerDB.db[user] = deleteElement(marks, i)
+			innerDB.amount--
 			return nil
 		}
 	}
@@ -83,7 +101,7 @@ func DelMark(user string, target int32) error {
 	return errors.New("target not found")
 }
 
-func deleteElement(markList []mark, i int) []mark {
+func deleteElement(markList []Mark, i int) []Mark {
 	if len(markList)-1 == i {
 		markList = markList[:i]
 	} else {
